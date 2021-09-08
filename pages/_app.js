c9eac468134bcfde73c3afd75d73f3ce90c19171
Becca Bailey <becca.bailey@formidable.com>
2021-09-08 15:25:09 -0700

    Remove urql client

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,5 @@
-import {withUrqlClient} from "next-urql";
-
 function MyApp({Component, pageProps}) {
   return <Component {...pageProps} />;
 }
 
-export default withUrqlClient(() => ({
-  url: "/api/graphql",
-}))(MyApp);
+export default MyApp;
