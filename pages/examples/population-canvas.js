04e8a90b7f20f013af6f716bf469254907061c59
Becca Bailey <becca.bailey@formidable.com>
2021-09-23 17:25:23 -0700

    Fix lodash import

--- pages/examples/population-canvas.js
+++ pages/examples/population-canvas.js
@@ -1,5 +1,5 @@
 import * as d3 from "d3";
-import {get} from "lodash";
+import get from "lodash/get";
 import React from "react";
 import styled from "styled-components";
 import {Tooltip} from "../../components/svg/Tooltip";
